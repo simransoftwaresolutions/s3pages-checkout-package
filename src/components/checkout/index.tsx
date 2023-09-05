6384eb1daa74e3c7b5002ce8883e02d09fddaa75
SS-Deepak <deepak@simransoftwaresolutions.com>
2023-09-05 17:20:22 +0530

    final commit

--- src/components/checkout/index.tsx
+++ src/components/checkout/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Head from "next/head";
 import { CheckoutPage } from "./components";
 import { ProductProvider } from "@/context/product/product.provider";
 import { ToastContainer } from 'react-toastify';
@@ -11,13 +10,6 @@ import 'react-toastify/dist/ReactToastify.css';
 export function CheckoutProduct({ data }: { data: any }) {
     return (
         <>
-            <Head>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"></link>
-                <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossOrigin="anonymous"></script>
-                <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossOrigin="anonymous"></script>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossOrigin="anonymous"></script>
-            </Head>
-
             <ToastContainer
                 pauseOnHover={false}
                 autoClose={1500}
