a1fde1f2f2f246b7846e0aca21b05d0a01de858d
SS-Deepak <deepak@simransoftwaresolutions.com>
2023-09-06 15:29:30 +0530

    styles updated

--- src/components/checkout/index.tsx
+++ src/components/checkout/index.tsx
@@ -6,6 +6,7 @@ import { ToastContainer } from 'react-toastify';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import "../../styles/globals.css"
 
 export function CheckoutProduct({ data }: { data: any }) {
     return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as ReactDOM from 'react-dom';
 import App from "./App"
-import "./styles/globals.css"
 
 ReactDOM.render(
   <App />,
