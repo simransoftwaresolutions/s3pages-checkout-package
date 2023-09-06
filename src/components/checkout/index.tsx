d1d7b8dd8d41d5ecd3e269d6199ec8c57901c16a
SS-Deepak <deepak@simransoftwaresolutions.com>
2023-09-06 16:01:15 +0530

    style added to public folder

--- src/components/checkout/index.tsx
+++ src/components/checkout/index.tsx
@@ -6,7 +6,6 @@ import { ToastContainer } from 'react-toastify';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import "../../styles/globals.css"
 
 export function CheckoutProduct({ data }: { data: any }) {
     return (
