79e212d2a9ba7d0ca8bfb08bfb22f10c225b25ef
SS-Deepak <deepak@simransoftwaresolutions.com>
2023-09-06 16:19:43 +0530

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,6 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
                 test: /\.(js|jsx|ts|tsx)$/,
                 exclude: /node_modules/,
                 use: {
