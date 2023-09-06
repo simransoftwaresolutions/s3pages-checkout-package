25d0e4b27bc09b19831e7a61542daf40f64d4847
SS-Deepak <deepak@simransoftwaresolutions.com>
2023-09-06 11:37:08 +0530

    changes

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(js|jsx|ts|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react'],
+                    },
+                },
+            },
+        ],
+    },
+};
