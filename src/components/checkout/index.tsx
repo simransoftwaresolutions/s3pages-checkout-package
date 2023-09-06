c695c27c6fecd013bd0cafff6ff845b0ea133e53
SS-Deepak <deepak@simransoftwaresolutions.com>
2023-09-06 11:40:33 +0530

    some changes

--- src/components/checkout/index.tsx
+++ src/components/checkout/index.tsx
@@ -4,8 +4,8 @@ import { CheckoutPage } from "./components";
 import { ProductProvider } from "../../context/product/product.provider";
 import { ToastContainer } from 'react-toastify';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 export function CheckoutProduct({ data }: { data: any }) {
     return (
