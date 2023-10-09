5f2a7ef21ae7288222e121d99d34b3f87277f33f
hardiksss <122963060+hardiksss@users.noreply.github.com>
2023-10-09 14:44:32 +0530

    Updates
    1. bootstrap issue

--- src/components/checkout/index.tsx
+++ src/components/checkout/index.tsx
@@ -4,9 +4,10 @@ import { CheckoutPage } from "./components";
 import { ProductProvider } from "../../context/product/product.provider";
 import { ToastContainer } from 'react-toastify';
 import { useGlobalCSS } from "../../helper/arrayToCss";
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
 import "../../styles/globals.css"
 
 export function CheckoutProduct({ product, style }: { product: any, style: any }) {
