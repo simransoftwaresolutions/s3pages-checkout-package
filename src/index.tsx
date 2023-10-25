33e47978603f7b662cbc2581f5b7fea74ae25772
rohit kumar <rohit@simransoftwaresolutions.com>
2023-10-25 12:47:54 +0530

    create theme

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+import "../src/styles/globals.css"
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
