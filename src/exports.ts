a9b3b85582fca03f05921cd7fa7dcf4fc63c6dd7
rohit kumar <rohit@simransoftwaresolutions.com>
2023-10-25 11:57:10 +0530

    add export

--- src/exports.ts
+++ src/exports.ts
@@ -1,8 +1,10 @@
 import { CheckoutProduct } from './components/checkout';
 import PreviewPage from './components/pagepreview';
 import Templategen from "../src/components/templategen/index"
+import PageTemplate from "../src/components/page-template/index"
 export {
     CheckoutProduct,
     PreviewPage,
     Templategen,
+    PageTemplate,
 }
