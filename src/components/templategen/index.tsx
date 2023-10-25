465a414b8695ba49cf270e280bb302af12f56f41
rohit kumar <rohit@simransoftwaresolutions.com>
2023-10-25 10:38:23 +0530

    create theme

--- src/components/templategen/index.tsx
+++ src/components/templategen/index.tsx
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
-// import Header from "../../../components/header/indesx";
+
 import Step1 from "./Step1";
 import Step2 from "./Step2";
 import Step3 from "./Step3";
-// import "./modal.css"
 export default function index() {
   const [step, setStep] = useState("step1");
   return (
     <div>
-      {/* <Header /> */}
+    
       {step === "step1" && <Step1 setStep={setStep} />}
       {step === "step2" && <Step2 setStep={setStep} />}
       {step === "step3" && <Step3 setStep={setStep} />}
