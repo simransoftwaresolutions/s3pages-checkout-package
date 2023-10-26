a4f8c847ced81f55d8f05f33bf8de483f73b8628
rohit kumar <rohit@simransoftwaresolutions.com>
2023-10-26 11:52:02 +0530

    updated

--- src/exports.ts
+++ src/exports.ts
@@ -1,7 +1,7 @@
 import { CheckoutProduct } from './components/checkout';
 import PreviewPage from './components/pagepreview';
-import Templategen from "./components/templategen/index"
-import PageTemplate from "./components/page-template/index"
+import Templategen from "./components/templategen"
+import PageTemplate from "./components/page-template"
 export {
     CheckoutProduct,
     PreviewPage,
