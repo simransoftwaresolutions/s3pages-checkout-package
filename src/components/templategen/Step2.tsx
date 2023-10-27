ae88247af32487e3fc09d9315a7c500558802c87
rohit kumar <rohit@simransoftwaresolutions.com>
2023-10-27 13:47:59 +0530

    chnage code in mui5 to bootstrap

--- src/components/templategen/Step2.tsx
+++ src/components/templategen/Step2.tsx
@@ -1,13 +1,4 @@
-import React from "react";
-import { Box, Button, IconButton } from "@mui/material";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import FmdGoodIcon from "@mui/icons-material/FmdGood";
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-export default function Step2({ setStep }:any) {
+export default function Step2({ setStep }: any) {
   const top100Films = [
     { label: "The Shawshank Redemption", year: 1994 },
     { label: "The Godfather", year: 1972 },
@@ -22,106 +13,54 @@ export default function Step2({ setStep }:any) {
     },
   ];
   return (
-    <Box sx={{ background: "#F6F6F6", height: "100%" }}>
-      <Container maxWidth="lg">
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "89vh",
+    <div className="bg-light" style={{ height: "100vh" }}>
+      <div
+        className="container-lg d-flex justify-content-center align-items-center"
+        style={{ height: "89vh" }}
+      >
+        <div
+          className="p-5 rounded-3 shadow bg-white"
+          style={{
+            width: "500px",
+            position: "relative",
+            borderRadius: "20px",
+            boxShadow:
+              "0px 1px 2px rgba(0, 0, 0, 0.07), 0px 2px 4px rgba(0, 0, 0, 0.07), 0px 4px 8px rgba(0, 0, 0, 0.07), 0px 8px 16px rgba(0, 0, 0, 0.07), 0px 16px 32px rgba(0, 0, 0, 0.07), 0px 32px 64px rgba(0, 0, 0, 0.07)",
           }}
         >
-          <Box
-            sx={{
-              padding: "70px 50px",
-              width: "500px",
-              position: "relative",
-              borderRadius: "20px",
-              backgroundColor: "#fff",
-              boxShadow:
-                " rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px;",
-            }}
+          <div className="text-center mb-4">
+            <i className="fa-sharp fa-solid fa-location-dot text-primary fs-3"></i>
+            <h6 className="mt-2">Where is your business located?</h6>
+          </div>
+          <button
+            className="btn btn-secondary btn-sm mb-3"
+            onClick={() => setStep("step1")}
           >
-            
-            <Box
-              sx={{
-                borderRadius: "100px",
-               
-                backgroundColor: "#fff",
-                display: "block",
-                textAlign:"center",
-                position:"relative",
-                top:"-20px",
-              }}
+           <i className="fa-solid fa-arrow-left"></i> Back
+          </button>
+          <div className="mb-3">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Coaching, Photography, Landscaping..."
+            />
+          </div>
+          <div className="mb-3 d-flex justify-content-between align-items-center">
+            <button
+              className="btn btn-warning text-secondary w-100"
+              onClick={() => setStep("step3")}
+              style={{ borderRadius: "50px" }}
             >
-              <FmdGoodIcon sx={{ color: "#9234e0", fontSize: "50px" }} />
-            </Box>
-            <Box pb={1}>
-              <IconButton
-                sx={{ borderRadius: "10px", color: "#492cdd" }}
-                onClick={() => setStep("step1")}
-              >
-                <ArrowBackIcon />
-                <Typography variant="body1" color="#492cdd" >
-                  Back
-                </Typography>
-              </IconButton>
-            </Box>
-            <Typography variant="h6">
-              Where is your business located?
-            </Typography>
-            <Box pt={2}>
-              <Autocomplete
-                id="free-solo-demo"
-                freeSolo
-                options={top100Films.map((option) => option.label)}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    placeholder="Coaching, Photography, Landscaping..."
-                  />
-                )}
-              />
-            </Box>
-            <Box>
-              <Button
-                className={`bg-warning  text-secondary footerButton`}
-                sx={{
-                  fontFamily: "sans-serif",
-                  textAlign: "center !important",
-                  backgroundColor: "#ffc300 !important",
-
-                  borderRadius: "105px !important",
-                  textDecoration: "none !important",
-                  fontSize: "15px !important",
-                  fontWeight: "600 !important",
-
-                  margin: "0 auto !important",
-                  lineHeight: "30px !important",
-                  marginTop: "40px !important",
-                  marginBottom: "20px !important",
-                  color: "#181723 !important",
-                  textTransform: "uppercase !important",
-                  padding: "0px",
-                  width: "100% !important",
-                  maxWidth: "100% !important",
-                  height: "50px",
-                }}
-                onClick={() => setStep("step3")}
-              >
-                Next <ArrowForwardIcon/>
-              </Button>
-            </Box>
-            <Box textAlign="center">
-              <Typography variant="body1">
-                Website language
-                <span style={{ color: "#492cdd" }}>English</span>
-              </Typography>
-            </Box>
-          </Box>
-        </Box>
-      </Container>
-    </Box>
+              Next <i className="fa-solid fa-arrow-right"></i>
+            </button>
+          </div>
+          <div className="text-center">
+            <p className="mb-0">
+              Website language <span className="text-primary">English</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
