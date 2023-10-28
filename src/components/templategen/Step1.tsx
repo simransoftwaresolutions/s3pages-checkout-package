112d671e6712afba9ca09ad4bb0a8b981a8e3e46
rohit kumar <rohit@simransoftwaresolutions.com>
2023-10-28 13:49:22 +0530

    fix changes

--- src/components/templategen/Step1.tsx
+++ src/components/templategen/Step1.tsx
@@ -1,5 +1,11 @@
-
-
+import React from "react";
+import { Box, Button } from "@mui/material";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 export default function Step1({ setStep }: any) {
   const top100Films = [
     { label: "The Shawshank Redemption", year: 1994 },
@@ -15,37 +21,95 @@ export default function Step1({ setStep }: any) {
     },
   ];
   return (
-    <div className="bg-light" style={{ height: "100vh" }}>
-    <div className="container-lg d-flex justify-content-center align-items-center" style={{ height: "90vh" }}>
-      <div className="p-5 rounded-3 shadow bg-white" style={{ width: "500px" }}>
-        <div className="text-center mb-4">
-          {/* <BusinessCenterIcon className="text-primary fs-3" /> */}
-          <i className="fas fa-briefcase text-primary fs-3"></i>
-          <h6 className="mt-2">What type of business are you building?</h6>
-        </div>
-        <div className="mb-3">
-          <input
-            className="form-control"
-            type="text"
-            placeholder="Coaching, Photography, Landscaping..."
-          />
-        </div>
-        <div className="mb-3">
-          <button
-            className="btn btn-warning text-secondary w-100"
-            style={{ borderRadius: "50px" }}
-            onClick={() => setStep("step2")}
+    <Box sx={{ background: "#F6F6F6", height: "100%" }}>
+      <Container maxWidth="lg">
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: { xs: "92.1vh", md: "89.4vh" },
+          }}
+        >
+          <Box
+            sx={{
+              padding: "70px 50px",
+              width: "500px",
+              borderRadius: "20px",
+              backgroundColor: "#fff",
+              position: "relative",
+              boxShadow:
+                " rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px;",
+            }}
           >
-            Next <i className="fa-solid fa-arrow-right"></i>
-          </button>
-          
-        </div>
-        <div className="text-center">
-          <p className="mb-0">Not sure? <span className="text-primary">See some suggestions</span></p>
-        </div>
-      </div>
-    </div>
-  </div>
-  
+            <Box
+              sx={{
+                borderRadius: "100px",
+
+                backgroundColor: "#fff",
+                display: "block",
+                textAlign: "center",
+                position: "relative",
+                top: "-20px",
+              }}
+            >
+              <BusinessCenterIcon sx={{ color: "#9234e0", fontSize: "50px" }} />
+            </Box>
+            <Typography variant="h6">
+              What type of business are you building?
+            </Typography>
+            <Box pt={2}>
+              <Autocomplete
+                id="free-solo-demo"
+                freeSolo
+                options={top100Films.map((option) => option.label)}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    placeholder="Coaching, Photography, Landscaping..."
+                  />
+                )}
+              />
+            </Box>
+            <Box>
+              <Button
+                className={`bg-warning  text-secondary footerButton`}
+                fullWidth
+                sx={{
+                  fontFamily: "sans-serif",
+                  textAlign: "center !important",
+                  backgroundColor: "#ffc300 !important",
+
+                  borderRadius: "105px !important",
+                  textDecoration: "none !important",
+                  fontSize: "15px !important",
+                  fontWeight: "600 !important",
+
+                  margin: "0 auto !important",
+                  lineHeight: "30px !important",
+                  marginTop: "40px !important",
+                  marginBottom: "20px !important",
+                  color: "#181723 !important",
+                  textTransform: "uppercase !important",
+                  padding: "0px",
+                  width: "100% !important",
+                  maxWidth: "100% !important",
+                  height: "50px",
+                }}
+                onClick={() => setStep("step2")}
+              >
+                Next <ArrowForwardIcon />
+              </Button>
+            </Box>
+            <Box textAlign="center">
+              <Typography variant="body1">
+                Not sure?
+                <span style={{ color: "#492cdd" }}> See some suggestions</span>
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
   );
 }
