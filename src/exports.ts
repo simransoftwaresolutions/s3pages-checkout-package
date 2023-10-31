38178fb33565a63397b913a407634f3eaca8c46f
rohit kumar <rohit@simransoftwaresolutions.com>
2023-10-31 16:53:29 +0530

    create banner section

--- src/exports.ts
+++ src/exports.ts
@@ -2,9 +2,13 @@ import { CheckoutProduct } from './components/checkout';
 import PreviewPage from './components/pagepreview';
 import Templategen from "./components/templategen"
 import PageTemplate from "./components/page-template"
+import Slidersection from "./components/slidersection"
+import Bannersection from "./components/bannersection"
 export {
     CheckoutProduct,
     PreviewPage,
     Templategen,
     PageTemplate,
+    Slidersection,
+    Bannersection,
 }
