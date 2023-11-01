b649065a91699e17de250702caadb78ecc0c19b0
SS-Deepak <deepak@simransoftwaresolutions.com>
2023-11-01 12:16:55 +0530

    mouse action removed from MainContent.tsx

--- src/components/pagepreview/components/MainContent/MainContent.tsx
+++ src/components/pagepreview/components/MainContent/MainContent.tsx
@@ -897,8 +897,8 @@ const MainContent = () => {
                     onDragOver={(event) => onDragOverFromSection(event, sIndex)} 
                     onDragStart={(event:any) => dragSection(event, sec, sIndex)} 
                     ref={(el) => (secRefs.current[sIndex] = el)}
-                    onMouseLeave={(ev:any) => onMouseLeaveFromSec(ev)} 
-                    onMouseOver={(ev:any) => onMouseOverFromSec(ev, sIndex, sectionTooltipStr)}
+                    // onMouseLeave={(ev:any) => onMouseLeaveFromSec(ev)} 
+                    // onMouseOver={(ev:any) => onMouseOverFromSec(ev, sIndex, sectionTooltipStr)}
                   >
                     {sectionHoverIdx === sIndex && actionSectionComponent(sectionTooltipStr, sec, sIndex)}
                     {
